d37f930edfe4a50c79bd23cc4082ea8e1d901c53
lazynormz <skupi1234@gmail.com>
2020-10-20 19:01:29 +0200

    Moved file to correct place and include<stddef.h>

--- kernel/include/kernel/tty.h
+++ kernel/include/kernel/tty.h
@@ -1,6 +1,8 @@
 #ifndef __KERNEL__TTY__H
 #define __KERNEL__TTY__H
 
+#include <stddef.h>
+
 void terminal_initialize(void);
 void terminal_putchar(char c);
 void terminal_write(const char* data, size_t size);
